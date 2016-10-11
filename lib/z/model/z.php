b4f5c80770d1751d968025ce9ad4cf00577ef72a
chenliujin <liujin.chen@qq.com>
2016-10-11 07:41:38 -0700

    update

--- lib/z/model/z.php
+++ lib/z/model/z.php
@@ -3,10 +3,11 @@
 include_once('chenliujin/mysql/Model.class.php');
 
 if (empty(Model::$dbo)) {
-	Model::$host = '192.168.145.129';
-	Model::$dbName = 'zzrig';
-	Model::$dbUser = 'zzrig';
-	Model::$dbPwd   = 'zzrig';
+	Model::$host 	= DB_SERVER;
+	Model::$dbName 	= DB_DATABASE;
+	Model::$dbUser 	= DB_SERVER_USERNAME;
+	Model::$dbPwd   = DB_SERVER_PASSWORD;
+
 	Model::$dbo = new Model;
 }
 
